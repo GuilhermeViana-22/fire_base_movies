af840db40d0d106adaa309a0df601ef3a1380246
GuilhermeViana-22 <gguicido.viana@gmail.com>
2024-07-26 16:18:30 -0300

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,10 @@
+import 'package:firebase_movies_app/core/config/firebase_movies_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FirebaseMoviesApp());
 }
 
-class MyApp extends StatelessWidget {
-  
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
